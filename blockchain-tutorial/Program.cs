1e2dbb22592fa5664f2b81fec38f5df5c2df18e7
Sebastian Tuyu <alextpineiro@gmail.com>
2020-10-14 17:21:57 -0500

    First changes

---
+++ blockchain-tutorial/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Console;
+
+namespace blockchain_tutorial
+{
+    class Program
+    {
+
+        class Block
+        {
+
+            //PROPIEDADES DE LA CLASE
+            public byte[] hash { get; set; }
+            
+            public string data { get; set; }
+            public int nonce { get; set; }
+
+
+            /*public Block(string hash, int nonce)
+            {
+                this.hash = hash;
+                this.nonce = nonce;
+            }*/
+
+            public byte[] generate_hash()
+            {
+                string complete = Convert.ToString(this.nonce) + this.data; 
+                byte[] c_byte = Encoding.ASCII.GetBytes(complete),conver;
+                SHA256 sha = SHA256.Create();
+                conver = sha.ComputeHash(c_byte);
+                return conver;
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+ 
+            Block ob1 = new Block();
+
+            ob1.data = "sebasTOrobert100";
+            ob1.nonce = 0;
+            ob1.hash = ob1.generate_hash();
+
+            for(int i = 0; i < ob1.hash.Length; i++)
+            {
+                Write($"{ob1.hash[i]:X2}");
+               
+            }
+            //
+
+        }
+
+
+    
+    }
+
+}
